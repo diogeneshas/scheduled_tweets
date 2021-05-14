ec60943062df47db5778e30a48699dfd10bbc365
diogeneshas <diogenes.henrique.br@gmail.com>
2021-05-14 10:28:00 -0300

    Edit Password

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@
 Rails.application.routes.draw do
   get "about", to: "about#index"
 
-  root "main#index"
+  get "password", to: "passwords#edit"
+  patch "password", to: "passwords#update", as: :edit_password
+  
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
@@ -12,4 +14,6 @@ Rails.application.routes.draw do
   post "sign_in", to: "sessions#create"
 
   delete "logout", to: "sessions#destroy"
+
+  root "main#index"
 end
